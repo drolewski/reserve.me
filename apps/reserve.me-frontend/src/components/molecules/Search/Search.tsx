fbdac3a7650c5eb8607aaaf260774979641cd546
Rolex <drolewski@gmail.com>
2023-05-28 01:15:16 +0200

    Fix UI and navigation on search screen

--- apps/reserve.me-frontend/src/components/molecules/Search/Search.tsx
+++ apps/reserve.me-frontend/src/components/molecules/Search/Search.tsx
@@ -1,4 +1,4 @@
-import {KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {useEffect, useState} from 'react';
 import {CompanyListResponse} from '../../../services/company/CompanyListResponse';
 import {getAllCompanies, getCategories} from '../../../services/company/CompanyService';
@@ -34,56 +34,60 @@ const Search = ({navigation}: any) => {
   }, [searchValue, searchCategory]);
 
   return <View style={{flex: 1}}>
-    <ScrollView keyboardShouldPersistTaps="handled"
-                contentContainerStyle={{
-                  alignContent: 'center',
-                  justifyContent: 'center'
-                }}>
-      <KeyboardAvoidingView enabled>
-        <View style={styles.sectionStyle}>
-          <TextInput
-            style={styles.inputStyle}
-            onChangeText={(searchBy) => setSearchValue(searchBy)}
-            placeholder="Enter company name"
-            placeholderTextColor="#8b9cb5"
-            autoCapitalize="sentences"
-            returnKeyType="next"
-            blurOnSubmit={false}
-          />
-        </View>
-        <View style={styles.sectionStyle}>
-          {
-            categories.map(category => <TouchableOpacity
-              style={category.name === searchCategory?.name ? styles.buttonStyle : styles.successTextStyle}
-              key={category.name}
-              activeOpacity={0.5}
-              onPress={() => {
-                if (searchCategory !== category) {
-                  setSearchCategory(category);
-                } else {
-                  setSearchCategory(undefined);
-                }
-              }}>
-              <Text>{category.name}</Text>
-            </TouchableOpacity>)
-          }
-        </View>
-        {displayCompanies.map(company =>
-          <TouchableOpacity
-            key={company.name}
-            activeOpacity={0.5}
-            onPress={() => navigation.navigate('SearchCompanyDetails', {companyName: company.name})}>
-            <View style={styles.serviceCategorySectionStyle}>
-              <Text>{company.name}</Text>
-              <Text>{company.contact.phoneNumber}</Text>
-              <Text>{company.contact.email}</Text>
-              <Text>{company.address.street}</Text>
-              <Text>{company.address.number}</Text>
-              <Text>{company.address.city}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      </KeyboardAvoidingView>
+    <View style={styles.sectionStyle}>
+      <TextInput
+        style={styles.inputStyle}
+        onChangeText={(searchBy) => setSearchValue(searchBy)}
+        placeholder="Enter company name"
+        placeholderTextColor="#8b9cb5"
+        autoCapitalize="sentences"
+        returnKeyType="next"
+        blurOnSubmit={false}
+      />
+    </View>
+    <ScrollView horizontal={true} contentContainerStyle={styles.horizontalScrollView} style={{maxHeight: 60}}>
+      {
+        categories.map(category => <TouchableOpacity
+          style={category.name === searchCategory?.name ? styles.searchCategorySelectedStyle : styles.searchCategoryStyle}
+          key={category.name}
+          activeOpacity={0.5}
+          onPress={() => {
+            if (searchCategory !== category) {
+              setSearchCategory(category);
+            } else {
+              setSearchCategory(undefined);
+            }
+          }}>
+          <Text>{category.name}</Text>
+        </TouchableOpacity>)
+      }
+    </ScrollView>
+    <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{display: 'flex', flexDirection: 'column'}}>
+      {displayCompanies.map(company =>
+        <TouchableOpacity
+          key={company.name}
+          activeOpacity={0.5}
+          style={styles.companyTailStyle}
+          onPress={() => navigation.navigate('Company Details', {companyName: company.name})}>
+          <View style={styles.serviceCategorySectionStyle}>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company
+              name: </Text>{company.name ?? ""}
+            </Text>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company
+              email: </Text>{company.contact?.email ?? ""}</Text>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company phone
+              number:</Text>{company.contact?.phoneNumber ?? ""}</Text>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company
+              street: </Text>{company.address?.street ?? ""}</Text>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company address
+              number: </Text>{company.address?.number ?? ""}</Text>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company post
+              code: </Text>{company.address?.postCode ?? ""}</Text>
+            <Text style={styles.companyTextStyle}><Text style={styles.companyInfo}>Company
+              city: </Text>{company.address?.city ?? ""}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
     </ScrollView>
   </View>
 }
@@ -126,6 +130,28 @@ const styles = StyleSheet.create({
     marginRight: 35,
     marginTop: 20,
     marginBottom: 20,
+  },
+  horizontalScrollView: {
+    display: 'flex',
+    alignItems: 'center',
+    maxHeight: 60
+  },
+  searchCategoryStyle: {
+    color: '#FFFFFF',
+    justifyContent: 'center',
+    height: 40,
+    borderRadius: 30,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  searchCategorySelectedStyle: {
+    backgroundColor: '#8b9cb5',
+    color: '#FFFFFF',
+    justifyContent: 'center',
+    height: 40,
+    borderRadius: 30,
+    marginLeft: 10,
+    marginRight: 10,
   },
   buttonTextStyle: {
     color: '#FFFFFF',
@@ -179,6 +205,18 @@ const styles = StyleSheet.create({
     marginLeft: 35,
     marginRight: 35,
   },
+  companyTextStyle: {
+    color: '#8b9cb5',
+    fontSize: 18,
+  },
+  companyInfo: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  companyTailStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#dadae8',
+  }
 })
 
 export default Search;
--- apps/reserve.me-frontend/src/components/molecules/Search/SearchNavigationStack.tsx
+++ apps/reserve.me-frontend/src/components/molecules/Search/SearchNavigationStack.tsx
@@ -2,21 +2,43 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Search from './Search';
 import SearchCompanyDetails from './SearchCompanyDetails';
 import Reservation from '../Reservation/Reservation';
+import {useNavigation} from '@react-navigation/native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator();
 
 const SearchNavigationStack = () => {
+
+  const navigation = useNavigation();
+
+  const headerOption = (url: string) => {
+    return {
+      headerLeft: () => (
+        <TouchableOpacity onPress={() => navigation.navigate(url)}>
+          <MaterialCommunityIcons style={styles.backButtonStyle} name="chevron-left" size={22}/>
+        </TouchableOpacity>
+      ),
+    }
+  }
   return <Stack.Navigator initialRouteName="SearchScreen">
     <Stack.Screen name="Search Company"
                   component={Search}
                   options={{headerShown: false}}/>
     <Stack.Screen name="Company Details"
                   component={SearchCompanyDetails}
-                  options={{headerShown: false}}/>
+                  options={headerOption("Search Company")}/>
     <Stack.Screen name="Reservation"
                   component={Reservation}
-                  options={{headerShown: false}}/>
+                  options={headerOption("Search Company")}/>
   </Stack.Navigator>
 }
+
+const styles = StyleSheet.create({
+  backButtonStyle: {
+    margin: 10
+  }
+});
 
 export default SearchNavigationStack;
